e281fb72899f29bfb7e72cdea0199d287616f570
ethansTJ <87584588+ethansTJ@users.noreply.github.com>
2021-07-21 11:19:03 -0500

    Create catch-exceptions.py

---
+++ 19-catch-exceptions.py
@@ -0,0 +1,5 @@
+try:
+    number = int(input("What is your favorite number? \n"))
+    print("Your favorite number times 6 is " + str(number * 6))
+except:
+    print("You didn't give me a number, did you?")
