290e9e79ca4aee8195538ca8f17c2d1d938866b4
ethansTJ <87584588+ethansTJ@users.noreply.github.com>
2021-07-21 11:11:17 -0500

    Create get-user-input-conditional-ignore-capitalization.py

---
+++ 7-get-user-input-conditional-ignore-capitalization.py
@@ -0,0 +1,8 @@
+answer = str(input("Do you like pizza? Answer yes or no. \n"))
+
+if (answer.lower() == "yes"):
+    print("Hey, me too.")
+elif (answer.upper() == 'NO'):
+    print("That's ok. More for me!")
+else:
+    print("You didn't follow the directions.")
